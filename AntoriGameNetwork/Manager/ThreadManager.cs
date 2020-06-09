aefa51ec41f658c9a130d56550616995718d5c97
Antori <an7222@naver.com>
2020-06-09 18:18:08 +0900

    test

---
+++ AntoriGameNetwork/Manager/ThreadManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+class ThreadManager : Singleton<ThreadManager> {
+    void RegisterWork(Action cb) {
+
+        ThreadPool.QueueUserWorkItem(new WaitCallback((object a) => {
+            cb();
+        }));
+    }
+}
+
--- AntoriGameNetwork/Program.cs
+++ AntoriGameNetwork/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-class Program
-{
-    static void Main(string[] args)
-    {
+class Program {
+    static void Main(string[] args) {
         Console.WriteLine("Hello World!");
     }
 }
