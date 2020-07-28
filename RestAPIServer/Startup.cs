f4d9deb366535206ed6e3abb6e6512761da570d2
Antori <an7222@naver.com>
2020-07-28 18:58:31 +0900

    메모리 DB로 수정

--- RestAPIServer/Startup.cs
+++ RestAPIServer/Startup.cs
@@ -23,7 +23,7 @@ public class Startup {
 
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services) {
-        services.AddDbContext<TodoContext>(opt => opt.UseSqlServer("TodoList"));
+        services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
 
         services.AddControllers();
     }
