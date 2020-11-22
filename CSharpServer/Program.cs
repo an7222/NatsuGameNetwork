bed88635ab916b0c4c2debb1768b6266a75b1368
AnSangYeol <an7222@naver.com>
2020-11-22 11:33:07 +0900

    프레임 제한 적용

--- CSharpServer/Program.cs
+++ CSharpServer/Program.cs
@@ -1,14 +1,28 @@
-﻿class Program {
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+class Program {
+    
     static void Main(string[] args) {
         ProtocolManager.GetInstance().Register();
         ProtocolHandler.GetInstance().Register();
         SessionServer.GetInstance().Start();
         BattleServer.GetInstance().Start();
 
+        var sw = new Stopwatch();
+        int frameLimit = 1000 / 120; // 1000ms / 120fps
         while (true) {
             foreach (var channelController in BattleServer.GetInstance().GetChannelControllerPool()) {
-                //TODO : Frame Sync
+                sw.Restart();
                 channelController.Update();
+                sw.Stop();
+
+                var elapsed = (int)sw.ElapsedMilliseconds;
+                var sleepDuration = frameLimit - elapsed;
+                if(sleepDuration > 0) {
+                    Thread.Sleep(sleepDuration);
+                }
             }
         }
     }
